019640f23baac9b06e86f0340890cca54c17366b
TRUE Wodkowski <hello@true.pl>
2015-07-03 12:16:58 +0200

    Commit

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+  if((/Android|iPhone|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)){
+  
+    $(".cycle-slideshow").removeClass("cycle-slideshow");
+  
+  }
+
+});
